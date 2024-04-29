4a1d16db614ae4d2b1653268ac535eb512a37b4d
Gael <prungael@gmail.com>
2024-04-29 23:15:03 +0200

    feat: add logger

--- p_doc/app.py
+++ p_doc/app.py
@@ -1,27 +1,60 @@
 """ Entry point for the application. """
 
 import argparse
+import logging
+from .utils import setup_logger
 from p_doc.config_manager import ConfigManager, Layer
 from p_doc.config_manager.default_config import DefaultConfig
 
 
-def init_config(parser):
-    """Initialize the configuration."""
-    config_manager = ConfigManager()
-    config_manager.add_layer(Layer("default", DefaultConfig.as_dict(), None))
-    config_manager.add_layer(Layer.load_from_args_parser(parser))
-    config_manager.compute_config()
-    return config_manager
+class AppManager:
+    """App manager class."""
+
+    def __init__(self):
+        """Initialize the AppManager."""
+        setup_logger("app.log", logging.DEBUG)
+
+        self.logger = logging.getLogger(__name__)
+         
+
+    def run(self):
+        """Run the application."""
+        self.logger.critical("Starting the application...")
+        try:
+            self._init()
+            self._scrap()
+            self._process()
+            self._generate()
+        except Exception as e:
+            self.logger.error("An error occured: %s", str(e))
+        finally:
+            self.logger.critical("Application finished.")
+
+    def _init(self):
+        pass
+        # config
+
+    def _scrap(self):
+        pass
+
+    def _process(self):
+        pass
+
+    def _generate(self):
+        pass
+
+# def init_config(parser):
+#     """Initialize the configuration."""
+#     config_manager = ConfigManager()
+#     config_manager.add_layer(Layer("default", DefaultConfig.as_dict(), None))
+#     config_manager.add_layer(Layer.load_from_args_parser(parser))
+#     config_manager.compute_config()
+#     return config_manager
 
 
 def main():
-    """Main function."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--output-path", default=None, help="Output path")
-
-    print("Hello World!")
-    print(init_config(parser).get_config())
-
+    appManager = AppManager()
+    appManager.run()
 
 if __name__ == "__main__":
     main()
