339c6fc96820925854df8293f2478000fdf69689
Gael <prungael@gmail.com>
2024-04-29 23:50:45 +0200

    feat: can generate configfile

--- p_doc/app.py
+++ p_doc/app.py
@@ -18,9 +18,16 @@ class AppManager:
         self.logger = logging.getLogger(__name__)
 
         self.parser = argparse.ArgumentParser(description="Process some integers.")
+        self.parser.add_argument(
+            "--generate-config-file",
+            action="store_true",
+            help="Generate a configuration file",
+        )
         for elem in DefaultConfig.as_dict():
             elem = elem.replace("_", "-")
-            self.parser.add_argument(f"--{elem}", type=str, help=f"Set the value of {elem}")
+            self.parser.add_argument(
+                f"--{elem}", type=str, help=f"Set the value of {elem}"
+            )
         self.args = self.parser.parse_args()
 
         self.config_manager = ConfigManager()
@@ -30,6 +37,11 @@ class AppManager:
         self.logger.critical("Starting the application...")
         try:
             self._init()
+            config = self.config_manager.get_config()
+            if config.get("generate_config_file"):
+                config.pop("generate_config_file")
+                self.generate_config_file()
+                return
             self._scrap()
             self._process()
             self._generate()
@@ -40,32 +52,38 @@ class AppManager:
 
     def _init(self):
         self.logger.debug("Initializing the application...")
+
         self.config_manager.add_layer(Layer("default", DefaultConfig.as_dict(), None))
+
         if os.path.exists("pdoconf.json"):
             self.logger.debug("Loading configuration from file pdoconf.json")
-            self.config_manager.add_layer(Layer.load_from_json_file("config_file", "pdoconf.json"))
+            self.config_manager.add_layer(
+                Layer.load_from_json_file("config_file", "pdoconf.json")
+            )
         else:
             self.logger.debug("No configuration file found.")
+
         self.config_manager.add_layer(Layer.load_from_args_parser(self.args))
         self.config_manager.compute_config()
         self.logger.debug("Configuration: %s", self.config_manager.get_config())
 
     def _scrap(self):
-        return
+        self.logger.debug("Scraping data...")
 
     def _process(self):
-        return
+        self.logger.debug("Processing data...")
 
     def _generate(self):
-        return
+        self.logger.debug("Generating doc...")
 
     def generate_config_file(self):
         """Generate a configuration file."""
-        return
+        self.logger.info("Generating configuration file...")
+        self.config_manager.save_to_json_file("pdoconf.json")
 
 
 def main():
-    """ Main function. """
+    """Main function."""
     app_manager = AppManager()
     app_manager.run()
 
--- p_doc/utils.py
+++ p_doc/utils.py
@@ -1,15 +1,18 @@
 """ Utility functions. """
+
 import logging
 
 
 def setup_logger(log_file=None, log_level=logging.INFO):
-    """ Setup the logger. """
-    logging.basicConfig(level=log_level, format='%(asctime)s - %(levelname)s - %(message)s')
+    """Setup the logger."""
+    logging.basicConfig(
+        level=log_level, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
     logger = logging.getLogger()
 
     if log_file:
         file_handler = logging.FileHandler(log_file)
         file_handler.setLevel(log_level)
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
